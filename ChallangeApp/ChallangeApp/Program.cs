0193a5a8c8f0dd49b7f872adf9c1c60d1e9cd5aa
Michał Walencik <michalovski912@gmail.com>
2024-09-19 23:30:12 +0200

    zadanie dzien 4

---
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -0,0 +1,20 @@
+﻿string name = "Andrzej";
+string gender = "male";
+int age = 15;
+
+if (gender == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (gender == "male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine();
+}
